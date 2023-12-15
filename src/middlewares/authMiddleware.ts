1aaed5f8d52db30c376fbc724abbab2092cffb77
Sammy Ferreira <sammyfe2021@gmail.com>
2023-12-15 18:36:07 -0300

    :sparkles: feat: middleware authorization

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { UnauthorizedError } from "../helpers/api-erros";
+import { userRepository } from "../repositories/userRepository";
+import jwt from 'jsonwebtoken'
+
+interface JwtPayload {
+  id: string
+}
+
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const { authorization } = req.headers
+
+    if (!authorization) throw new UnauthorizedError('Unauthorized')
+
+    const token = authorization.split(' ')[1]
+
+    const { id } = jwt.verify(token, process.env.JWT_PASS as string) as JwtPayload
+
+    const userExist = await userRepository.findOneBy({ id: Number(id) })
+
+    if (!userExist) throw new UnauthorizedError('Unauthorized') 
+    
+    const {password: _, ...user } = userExist
+
+    req.user = user
+
+    next()
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,15 @@ import { Router } from 'express'
 import { UserController } from './controllers/UserController'
 import { LoginController } from './controllers/LoginController'
 import { ProfileController } from './controllers/ProfileController'
+import { authMiddleware } from './middlewares/authMiddleware'
 
 const routes = Router()
 
 routes.post('/users', new UserController().create)
 routes.post('/login', new LoginController().login)
+
+routes.use(authMiddleware)
+
 routes.get('/profile', new ProfileController().getProfile)
 
 export default routes
