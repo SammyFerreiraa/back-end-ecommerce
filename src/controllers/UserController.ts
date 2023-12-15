ebd5aa39ce66a21f85c9196985f16f9e9d7db783
Sammy Ferreira <sammyfe2021@gmail.com>
2023-12-15 17:26:58 -0300

    :sparkles: feat: create user

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { userRepository } from "../repositories/userRepository";
+import { BadRequestError } from "../helpers/api-erros";
+import bcrypt from 'bcrypt'
+
+export class UserController {
+  async create(req: Request, res: Response) {
+    const { name, email, password } = req.body
+
+    const userExists = await userRepository.findOne({ where: { email } })
+
+    if (userExists) {
+      throw new BadRequestError('Email already exists')
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    const newUser = userRepository.create({
+      name,
+      email,
+      password: hashPassword
+    })
+
+    await userRepository.save(newUser)
+
+  const { password: _, ...user } = newUser
+
+    return res.status(201).json(user)
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express'
+import { UserController } from './controllers/UserController'
 
 const routes = Router()
+
+routes.post('/users', new UserController().create)
 
 export default routes
