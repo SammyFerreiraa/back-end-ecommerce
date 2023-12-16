36be1656ecbaf68718fc0601129735f63b815f7c
Sammy Ferreira <sammyfe2021@gmail.com>
2023-12-16 11:21:13 -0300

    :sparkles: feat: create product and get product router

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,14 @@ import { UserController } from './controllers/UserController'
 import { LoginController } from './controllers/LoginController'
 import { ProfileController } from './controllers/ProfileController'
 import { authMiddleware } from './middlewares/authMiddleware'
+import { productController } from './controllers/ProductController'
 
 const routes = Router()
 
 routes.post('/users', new UserController().create)
 routes.post('/login', new LoginController().login)
+routes.post('/products', new productController().create)
+routes.get('/products', new productController().getProducts)
 
 routes.use(authMiddleware)
 
