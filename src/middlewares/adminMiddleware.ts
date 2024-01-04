9e206fe41e70e938fa7cf22cca476794f51171cc
Sammy Ferreira <sammyfe2021@gmail.com>
2024-01-04 17:56:27 -0300

    :recycle: refactor: unutilized deps

--- src/middlewares/adminMiddleware.ts
+++ src/middlewares/adminMiddleware.ts
@@ -1,9 +1,7 @@
 import { NextFunction } from "express"
-import { JwtPayload } from "jsonwebtoken"
 import { UnauthorizedError } from "../helpers/api-erros"
-import { userRepository } from "../repositories/userRepository"
 import { Request, Response } from "express"
-import jwt from 'jsonwebtoken'
+
 
 export const adminMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   if (!req.user.admin) throw new UnauthorizedError('Unauthorized')
